b1fe82922d5f15f29a0cc1cf81e7a36887191b1e
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-05-16 21:12:16 +0300

    Fixed typo.

--- server/server.js
+++ server/server.js
@@ -10,5 +10,5 @@ var app = express();
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}.`
+  console.log(`Listening on port ${port}.`);
 });
