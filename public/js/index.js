39886c1887cd87797b24022076eec4d619c87c83
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-05-19 18:09:54 +0300

    Added functionality for automatic scrolling in chat.

--- public/js/index.js
+++ public/js/index.js
@@ -10,12 +10,12 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   var msgtxt = Mustache.render(jQuery('#message-text-template').html(), {text : message.text});
-  jQuery('#messages').append(newMessageBody(message.from, message.createdAt, msgtxt));
+  postNewMessage(newMessageBody(message.from, message.createdAt, msgtxt));
 });
 
 socket.on('newLocationMessage', function (message) {
   link = Mustache.render(jQuery('#message-location-template').html(), {url: message.url});
-  jQuery('#messages').append(newMessageBody(message.from, message.createdAt, link));
+  postNewMessage(newMessageBody(message.from, message.createdAt, link));
 });
 
 jQuery('#message-form').on('submit', function (event) {
@@ -29,13 +29,33 @@ jQuery('#message-form').on('submit', function (event) {
   });
 });
 
+var postNewMessage = function (html) {
+  jQuery('#messages').append(html);
+  scrollToBottom();
+};
+
+var distanceToBottom = function () {
+  var messages = jQuery('#messages');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  return scrollHeight - clientHeight - scrollTop;
+}
+
+var scrollToBottom = function () {
+  var newMsgHeight = jQuery('#messages').children('li:last-child').innerHeight();
+  if (distanceToBottom() === newMsgHeight) {
+    console.log('Scroll!!');
+  }
+};
+
 var newMessageBody = function (from, timestamp, messageHtml) {
   return Mustache.render(jQuery('#message-template').html(), {
     from,
     createdAt: moment(timestamp).format("h:mm A"),
     messageHtml
   });
-}
+};
 
 var locationButton = jQuery('#send-location')
 locationButton.on('click', function () {
