728319fbb06ea25720e02b19f6e66b258c71ce3d
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-05-20 00:43:38 +0300

    Updated location sending and blocked the sending of empty messages.

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message, acknowledge) => {
       user = users.getUser(socket.id);
-      if (user) {
+      if (user && isRealString(message.text)) {
         io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
         acknowledge();
       } else {
@@ -48,7 +48,10 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    user = users.getUser(socket.id);
+    if (user) {
+      io.emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
